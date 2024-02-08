79484725696584ff04abd127631e0678ff57a2f1
Mohammed Nurul Islam <mohammed19.islam@gmail.com>
2024-02-08 21:15:04 +0000

    added data to readme and updated settings.py in stockApp

--- StockApp/StockApp/settings.py
+++ StockApp/StockApp/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api_app',
+    'sentiment_analysis_app',
+    'stock_tracker_app',
+    'stocks_management_app',
+    'user_auth_and_profiles'
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
