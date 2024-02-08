031762d9605ef1a3c0ca669897439dc2836c954e
Mohammed Nurul Islam <mohammed19.islam@gmail.com>
2024-02-08 22:17:13 +0000

    added test.py, signals.py and apps.py to stocks_management_app

--- StockApp/stocks_management_app/apps.py
+++ StockApp/stocks_management_app/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
 class StocksManagementAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'stocks_management_app'
+    
+    def ready(self):
+        from . import signals
+
