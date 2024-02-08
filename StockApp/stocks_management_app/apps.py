970fa5e254719146d6a933999244c6c394d01c1d
Mohammed Nurul Islam <mohammed19.islam@gmail.com>
2024-02-08 20:36:41 +0000

    created the new StockApp folder with all the app_names I will use

---
+++ StockApp/stocks_management_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StocksManagementAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'stocks_management_app'
---
+++ StockApp/user_auth_and_profiles/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UserAuthAndProfilesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'user_auth_and_profiles'
