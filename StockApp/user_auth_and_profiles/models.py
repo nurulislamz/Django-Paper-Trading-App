c2f70efbda4c2d4ad8baf64b27b65495a30cf2b2
Mohammed Nurul Islam <mohammed19.islam@gmail.com>
2024-02-26 17:22:27 +0000

    created profile page, can edit profile page, only change lefts are to configure email host, port and backend

--- StockApp/user_auth_and_profiles/models.py
+++ StockApp/user_auth_and_profiles/models.py
@@ -3,8 +3,11 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name = "profiles")
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name = "profile")
     bio = models.TextField()
     
     def __str__(self):
         return self.user.username
+    
+    def save(self, *args, **kwargs):
+        super().save()
--- StockApp/user_auth_and_profiles/urls.py
+++ StockApp/user_auth_and_profiles/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from .views import home, sign_up_view, login_view #UserEditForm
-from django.contrib.auth.views import LogoutView
-from .forms import LoginForm
+from .views import home, sign_up_view, login_view, logout_view, profile_view, reset_password_view, change_password_view #UserEditForm
+from django.contrib.auth.views import PasswordResetConfirmView, LogoutView
 
 urlpatterns = [
     path('', home, name='home_page'),
     path('signup/', sign_up_view.as_view(), name='sign_up_page'),
     path('login/', login_view.as_view(), name='login_page'),
-    path('logout/', LogoutView.as_view(template_name='user_auth_and_profiles/logout.html'), name='logout_page'),
+    path('logout/', logout_view.as_view(), name='logout_page'),
+    path('profile/', profile_view, name='profile_page'),
+    path('password_reset/', reset_password_view.as_view(), name='password_reset'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='user_auth_and_profils/password_reset_confirm.html'),name='password_reset_confirm'),    
+    path('password-change/', change_password_view.as_view(), name='password_change'),
     # path('edit_profile/', UserEditForm, name='edit_profile')
 ]
