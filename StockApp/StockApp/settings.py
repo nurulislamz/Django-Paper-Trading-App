0c1ae332bfcd7ed426079afb69d6be7aa5114e49
Mohammed Nurul Islam <mohammed19.islam@gmail.com>
2024-02-28 16:44:43 +0000

    changed to project level templates

--- StockApp/StockApp/settings.py
+++ StockApp/StockApp/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'StockApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
