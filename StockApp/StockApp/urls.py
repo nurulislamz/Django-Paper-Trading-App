a7e6532afe8207c03d4c3a00b75f29b290bac963
Mohammed Nurul Islam <mohammed19.islam@gmail.com>
2024-03-06 18:54:02 +0000

    test and url setup

--- StockApp/StockApp/urls.py
+++ StockApp/StockApp/urls.py
@@ -5,6 +5,7 @@ URL configuration for StockApp project.
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user_auth_and_profiles.urls'), name = "user_auth"),
--- StockApp/user_auth_and_profiles/tests.py
+++ StockApp/user_auth_and_profiles/tests.py
@@ -1,71 +1,86 @@
 # Create your tests here.
-from django.test import TestCase
-
-# Create your tests here.
 from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
-
+from django.urls import reverse
+from django.contrib.auth.models import User
 from .views import home_view
 from .models import Profile
+
+class NavigationBarTestCase(TestCase):
+    def test_navigation_bar_loaded(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'navbar')
+    
+    def test_navigation_links_for_unauthenticated_user(self):
+        response = self.client.get(reverse('home_page'))
+        self.assertContains(response, 'Home')
+    
+    # def test_navigation_links_for_authenticated_user(self):
+    #     self.client.login(username='admin', password='abc123')
+        
+    #     response = self.client.get(reverse('home_page'))
+    #     self.assertContains(response, 'Profile')
+
 
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'user_auth_and_profiles/home.html')
-        
-    def test_navigation_bar_functionality(self):
-        response = self.client.get('/')
-        self.assertContains(response, 'Home')
     
-    def test_authenticated_nav_bar(self):
-        self.client.login(username='user', password='pass')
-        response = self.client.get('/')
-        self.assertContains(response, 'Logout')
-    
-# class LoginTest(TestCase):
+class LoginTest(TestCase):
 
-#     def test_valid_input(self):
-#         pass
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.login_url = reverse('login_page')  # Replace 'login' with the actual name of your login URL
     
-#     def test_show_error_message(self):
-#         pass
-    
-#     def test_submit_button(self):
-#         pass
-    
-#     def test_redirects_after_POST(self):
-#         pass
-    
-# class SignupTest(TestCase):
+    def test_valid_input(self):
+        response = self.client.post(self.login_url, {'username': 'testuser', 'password': '12345'})
+        self.assertRedirects(response, reverse('home_page'))  # Replace 'expected_url' with the URL you expect to redirect to after login
 
-#     def test_valid_input(self):
-#         pass
+    def test_show_error_message(self):
+        response = self.client.post(self.login_url, {'username': 'wronguser', 'password': 'wrongpass'})
+        self.assertFormError(response, 'form', None, 'Invalid username or password')  # Adjust the error message to match what your form returns
+    
+    def test_submit_button(self):
+        response = self.client.get(self.login_url)
+        self.assertContains(response, '<input type="submit"')  # Check for the submit button in the form
+    
+    def test_redirects_after_POST(self):
+        response = self.client.post(self.login_url, {'username': 'testuser', 'password': '12345'}, follow=True)
+        self.assertRedirects(response, '/')  # Replace 'expected_url' with the URL you expect to redirect to after login
+    
+class SignupTest(TestCase):
 
-#     def test_validate_password(self):
-#         pass
+    def setup(self):
+        pass
     
-#     def test_duplicate_username(self):
-#         pass
+    def test_valid_input(self):
+        pass
+
+    def test_validate_password(self):
+        pass
     
-#     def test_duplicate_email(self):
-#         pass
+    def test_duplicate_username(self):
+        pass
     
-#     def test_submit_button(self):
-#         pass
+    def test_duplicate_email(self):
+        pass
     
-#     def test_redirects_after_POST(self):
-#         pass
+    def test_submit_button(self):
+        pass
     
-#     def test_deposits_saves_to_profile(self):
-#         pass
+    def test_redirects_after_POST(self):
+        pass
     
-# class ProfilePageTest(TestCase):
-#     def test_edit_username_valid(self):
-#         pass
+    def test_deposits_saves_to_profile(self):
+        pass
     
-#     def test_edit_username_invalid(self):
-#         pass
+class ProfilePageTest(TestCase):
+    def test_edit_username_valid(self):
+        pass
+    
+    def test_edit_username_invalid(self):
+        pass
       
-#     def test_redirects_after_POST(self):
-#         pass
+    def test_redirects_after_POST(self):
+        pass
