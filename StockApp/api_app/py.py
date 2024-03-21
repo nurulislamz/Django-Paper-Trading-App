a5d6a925d3d02410f1fd93d165feb23e3947300d
Mohammed Nurul Islam <mohammed19.islam@gmail.com>
2024-03-21 02:38:02 +0000

    created api requests for code

--- StockApp/api_app/py.py
+++ StockApp/api_app/py.py
@@ -1,17 +1,11 @@
 # testing apis
 import requests
-    
+
 # test    
 # historical data using REST Apis
 ## json 
 ## csv
-headers = {
-        'Content-Type': 'application/json',
-        'Authorization' : 'Token 7eb78fa4eb950d98a130935a693d016ea738cedf'
-        }
-requestResponse = requests.get("https://api.tiingo.com/api/test/",
-                                    headers=headers)
-print(requestResponse.json())
+
 
 
 # get historical data
@@ -39,6 +33,57 @@ print(requestResponse.json())
 requestResponse = requests.get("https://api.tiingo.com/tiingo/daily/aapl", headers=headers)
 print(requestResponse.json())
 
+# news data
+
+# For the latest news
+"https://api.tiingo.com/tiingo/news"
+
+# For the latest news for specific tickers
+"https://api.tiingo.com/tiingo/news?tickers=aapl,googl"
+
+# For the latest news for specific tags/countries/topics/tc
+"https://api.tiingo.com/tiingo/news?tags=election,argentina"
+
+# For a list of all the bulk download files
+"https://api.tiingo.com/tiingo/news/bulk_download"
+
+# To download a specific batch file
+"https://api.tiingo.com/tiingo/news/bulk_download/{id}"
+
+
+# Crypto
+
+# Top-of-Book Data for specific tickers
+"https://api.tiingo.com/tiingo/crypto/top?tickers=btcusd,fldcbtc"
+
+# Real-time (Latest) Data for specific tickers
+"https://api.tiingo.com/tiingo/crypto/prices?tickers=btcusd,fldcbtc"
+
+# Historical Prices
+"https://api.tiingo.com/tiingo/crypto/prices?tickers=btcusd,fldcbtc&startDate=2019-01-02&resampleFreq=5min"
+
+# To request meta data for all tickers, use the following REST endpoint
+"https://api.tiingo.com/tiingo/crypto"
+    
+# To request meta data for a specific tickers, use the following REST endpoint
+"https://api.tiingo.com/tiingo/crypto?tickers=<ticker>"
+
+# To see the available fundamental metrics, use this endpoint
+"https://api.tiingo.com/tiingo/fundamentals/definitions"
+
+# To request historical statement data, use this endpoint
+"https://api.tiingo.com/tiingo/fundamentals/<ticker>/statements"
+
+# To request historical daily fundamental data, use this endpoint
+"https://api.tiingo.com/tiingo/fundamentals/<ticker>/daily"
+
+# To request fundamental meta data, use this endpoint
+"https://api.tiingo.com/tiingo/fundamentals/meta"
+
+
+#dividends
+
+
 # websocket for real time stream data             
 
 from websocket import create_connection
@@ -55,4 +100,18 @@ subscribe = {
 ws.send(json.dumps(subscribe))
 for i in range(2):
     print(ws.recv())
-                        
+    
+# iex live ticker data
+ws = create_connection("wss://api.tiingo.com/iex")
+
+subscribe = {
+        'eventName':'subscribe',
+        'authorization':'7eb78fa4eb950d98a130935a693d016ea738cedf',
+        'eventData': {
+            'thresholdLevel': 5
+    }
+}
+
+ws.send(json.dumps(subscribe))
+while True:
+    print(ws.recv())
---
+++ StockApp/api_app/services.py
@@ -0,0 +1,83 @@
+import requests
+from websocket import create_connection
+import simplejson as json
+import datetime
+import asyncio 
+
+from django.core.exceptions import FieldDoesNotExist
+from django.core.validators import EMPTY_VALUES
+
+class tiingoAPI:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.base_url = "https://api.tiingo.com/tiingo/"
+        
+    def __test__(self):
+        headers = {
+                    'Content-Type': 'application/json',
+                    'Authorization' : 'Token' + self.apikey
+                  }
+        
+        requestResponse = requests.get("https://api.tiingo.com/api/test/", headers=headers)
+        print(requestResponse.json())
+        
+class tiingoStockAPI(tiingoAPI):
+    def __init__(self, ticker):
+        self.ticker = ticker
+        
+    async def get_historical_data(self, startDate, endDate, resampleFreq, responseFormat, columns):
+        response = requests.get(f"https://api.tiingo.com/tiingo/daily/{self.ticker}/prices?startDate={startDate}&format={responseFormat}", 
+                                headers=headers)
+        return response.json()
+    
+    # get todays data
+    async def get_current_price(self):
+        while running:
+            await asyncio.wait(30)
+            response = requests.get("https://api.tiingo.com/tiingo/daily/{self.ticker}/prices", 
+                                    headers=headers)
+            return response.json()
+        
+    async def get_metadata(self):
+        response = requests.get("https://api.tiingo.com/tiingo/daily/{self.ticker}", headers=headers)
+        print(response.json())
+        
+class tiingoNewsAPI(tiingoAPI):
+    def __init__(self):
+        self.base_url = "https://api.tiingo.com/tiingo/news"
+
+    async def get_latest_news(self):
+        response = requests.get("https://api.tiingo.com/tiingo/news")
+        return response.json()
+        
+    async def get_latest_news_for_tickers(self):
+        response = requests.get("https://api.tiingo.com/tiingo/news?tickers=aapl,googl")
+        return response.json()
+
+    async def get_latest_news_for_countries(self):
+        response = requests.get("https://api.tiingo.com/tiingo/news?tags=election,argentina")
+        return response
+    
+    async def download_news(self):
+        response = requests.get("https://api.tiingo.com/tiingo/news/bulk_download")
+        return response
+
+class tiingoCryptoAPI(self):
+    def __init__(self, tickers):
+        self.ticker = ticker
+        
+    async def get_top_of_book_data(self):
+        response = requests.get("https://api.tiingo.com/tiingo/crypto/top?tickers=btcusd,fldcbtc")
+
+    async def get_current_data(self):
+        response = requests.get("https://api.tiingo.com/tiingo/crypto/prices?tickers=btcusd,fldcbtc")
+        
+    async def get_historical_data(self):
+        response = requests.get("https://api.tiingo.com/tiingo/crypto/prices?tickers=btcusd,fldcbtc&startDate=2019-01-02&resampleFreq=5min")
+
+    async def get_metadata_for_all_tickers(self):
+        response = requests.get("https://api.tiingo.com/tiingo/crypto")
+
+    async def get_metadata_for_specific_ticker(self):
+        response = requests.get("https://api.tiingo.com/tiingo/crypto?tickers=<ticker>")
+
